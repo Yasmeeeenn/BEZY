3b0c07b3ab029e4f01b282046c86b4ed908108ff
Zac Wheeler <wheelerzac@yahoo.com>
2023-06-26 18:54:41 -0500

    added images

--- public/js/login.js
+++ public/js/login.js
@@ -25,6 +25,7 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  console.log(username, email, password);
 
   if (username && email && password) {
     const response = await fetch('/api/users', {
