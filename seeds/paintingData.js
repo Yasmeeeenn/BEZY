8c5f88a6180a9a46f2d5f25bd6bc8320383be339
Zac Wheeler <wheelerzac@yahoo.com>
2023-06-26 22:39:48 -0500

    added to css

--- seeds/paintingData.js
+++ seeds/paintingData.js
@@ -7,7 +7,7 @@ const paintingdata = [
     //difficulty level should replace artist
     exhibition_date: 'March 30, 2018',
     gallery_id: 1,
-    filename: 'close-grip-pushup.jpg',
+    filename: 'Barbell-Bench-Press.gif',
     description:
       'Branches with pink apricot blossoms against a blue background.',
   },
@@ -25,7 +25,7 @@ const paintingdata = [
     artist: 'Difficulty Level: Moderate',
     exhibition_date: 'May 05, 2017',
     gallery_id: 1,
-    filename: 'dips-1.jpg',
+    filename: 'dips.jpg',
     description: 'Pink cosmos flowers against a blue sky.',
   },
   {
@@ -33,7 +33,7 @@ const paintingdata = [
     artist: 'Difficulty Level: Hard',
     exhibition_date: 'June 10, 2019',
     gallery_id: 2,
-    filename: 'squat.webp',
+    filename: 'squat.jpg',
     description: 'Sandy beach with the blue sea and sky in the background.',
   },
   {
@@ -49,24 +49,24 @@ const paintingdata = [
     artist: 'Difficulty Level: Moderate',
     exhibition_date: 'August 14, 2016',
     gallery_id: 2,
-    filename: 'lunges.png',
+    filename: 'lunges.jpg',
     description: 'Sun setting in the horizon with waves lapping the shore.',
-  },
-  {
-    title: 'Deadlift',
-    artist: 'Difficulty Level: Hard',
-    exhibition_date: 'October 15, 2018',
-    gallery_id: 3,
-    filename: 'dead-lift.webp',
-    description:
-      'Trees with red, orange, yellow leaves reflected on a still lake.',
   },
   {
     title: 'Pull-ups',
     artist: 'Difficulty Level: Hard',
     exhibition_date: 'November 3, 2016',
     gallery_id: 3,
-    filename: 'pull-up.webp',
+    filename: 'pull-up.jpg',
+    description:
+      'Mountains with red and yellow leaves against a background of hazy rolling hills.',
+  },
+  {
+    title: 'Lat Pulldown',
+    artist: 'Difficulty Level: Hard',
+    exhibition_date: 'November 3, 2016',
+    gallery_id: 3,
+    filename: 'lat-pulldown.jpg',
     description:
       'Mountains with red and yellow leaves against a background of hazy rolling hills.',
   },
@@ -80,29 +80,57 @@ const paintingdata = [
       'Mountains with red and yellow leaves against a background of hazy rolling hills.',
   },
   {
-    title: 'Frozen River',
-    artist: 'Vlad Sokolovsky',
+    title: 'Skull Crusher',
+    artist: 'Difficulty Level: Moderate',
     exhibition_date: 'December 24, 2020',
     gallery_id: 4,
-    filename: '08-frozen-river.jpg',
+    filename: 'skull-crusher.jpg',
     description:
       'Trees with white frozen branches reflected on a frozen river against a light pink sky.',
   },
   {
-    title: 'Winter Home',
-    artist: 'Smit',
+    title: 'Barbell Curl',
+    artist: 'Difficulty Level: Easy',
     exhibition_date: 'January 20, 2018',
     gallery_id: 4,
-    filename: '09-winter-home.jpg',
+    filename: 'barbell-curl.jpg',
     description:
       'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
   },
   {
-    title: 'Winter Home',
-    artist: 'Smit',
+    title: 'Hammer Curl',
+    artist: 'Difficulty Level: Easy',
     exhibition_date: 'January 20, 2018',
     gallery_id: 4,
-    filename: '09-winter-home.jpg',
+    filename: 'hammer-curl.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+
+  {
+    title: 'Cable Crunch',
+    artist: 'Difficulty Level: Easy',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 5,
+    filename: 'cable-crunch.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+  {
+    title: 'Sit-Ups',
+    artist: 'Difficulty Level: Easy',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 5,
+    filename: 'sit-up.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+  {
+    title: 'Leg Lifts',
+    artist: 'Difficulty Level: Easy',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 5,
+    filename: 'leg-lifts.jpg',
     description:
       'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
   },
