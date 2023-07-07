b027a41a9140168a3edbc0edf7f42f79a0765350
Brooke Adlesperger-Jones <badles@live.com>
2023-07-06 22:13:20 -0500

    updated image files

--- seeds/exerciseData.js
+++ seeds/exerciseData.js
@@ -12,6 +12,16 @@ const exerciseData = [
       "The bench press is a classic strength training exercise targeting the muscles of your chest, shoulders, and triceps to build upper body strength and muscle mass.",
   },
   {
+    title: "Svend Press",
+    difficulty: "Moderate",
+    exhibition_date: "July 4, 2020",
+    muscleGroup_id: 1,
+    filename: "svend-press.png",
+    motion_file: "svend-press.gif",
+    description:
+      "The Svend press is an exercise where you hold two plates together in front of your chest and press them outward, engaging the chest and shoulder muscles while promoting muscular endurance and stability in the upper body.",
+  },
+  {
     title: "Cable Crossover",
     difficulty: "Easy",
     exhibition_date: "July 4, 2020",
@@ -82,16 +92,6 @@ const exerciseData = [
       "Dumbbell flys are an exercise where you lie on a bench and, with a dumbbell in each hand, bring your arms out to the sides in a wide arc motion, targeting the chest muscles.",
   },
   {
-    title: "Dumbbell Pull-Over",
-    difficulty: "Moderate",
-    exhibition_date: "July 4, 2020",
-    muscleGroup_id: 1,
-    filename: "dumbbell-pullover.jpg",
-    motion_file: "dumbbell-pullover.gif",
-    description:
-      "Dumbbell pull-overs are an exercise where you lie on a bench, hold a dumbbell above your chest, and lower it behind your head in a controlled motion, engaging the chest, back, and shoulder muscles.",
-  },
-  {
     title: "Front Raise",
     difficulty: "Moderate",
     exhibition_date: "July 4, 2020",
@@ -136,17 +136,77 @@ const exerciseData = [
     difficulty: "Hard",
     exhibition_date: "June 10, 2019",
     muscleGroup_id: 2,
-    filename: "straight-leg-deadlift.jpg",
+    filename: "straight-leg-deadlift.png",
     motion_file: "straight-leg-deadlift.gif",
     description:
       "The straight leg deadlift is a strength training exercise primarily targeting the hamstrings and glutes while also engaging the lower back and core for stability.",
   },
   {
-    title: "Pull-ups",
+    title: "Jump Squats",
+    difficulty: "Moderate",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "jump-squats.jpg",
+    motion_file: "jump-squats.gif",
+    description:
+      "Jump squats are a plyometric exercise where you perform a squat and explosively jump up, engaging the lower body muscles while improving power and cardiovascular fitness.",
+  },
+  {
+    title: "Lunge Step-Up",
+    difficulty: "Hard",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "lunge-step-up.png",
+    motion_file: "lunge-step-up.gif",
+    description:
+      "Lunge step-ups are a compound exercise where you step onto an elevated platform with one foot, then drive the opposite knee up towards the chest, targeting the leg muscles and improving balance and stability.",
+  },
+  {
+    title: "Pistol Squat",
+    difficulty: "Hard",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "pistol-squat.jpg",
+    motion_file: "pistol-squat.gif",
+    description:
+      "Pistol squats are a challenging single-leg exercise where you balance on one leg while squatting down to a deep position, effectively targeting the leg muscles and enhancing strength, stability, and mobility.",
+  },
+  {
+    title: "Split Squat",
+    difficulty: "Moderate",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "split-squat.jpg",
+    motion_file: "split-squat.gif",
+    description:
+      "Split squats, also known as static lunges, are a lower body exercise where one foot is positioned forward and the other foot is positioned back, targeting the quadriceps, glutes, and hamstrings while improving balance and stability.",
+  },
+  {
+    title: "Sumo Squat",
+    difficulty: "Moderate",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "sumo-squat.jpg",
+    motion_file: "sumo-squat.gif",
+    description:
+      "Sumo squats are a variation of the squat exercise where you stand with your feet wider than hip-width apart, toes pointed outward, and perform a squat, targeting the inner thighs, glutes, and quadriceps while enhancing lower body strength and stability.",
+  },
+    {
+    title: "Glute Bridge",
+    difficulty: "Easy",
+    exhibition_date: "June 10, 2019",
+    muscleGroup_id: 2,
+    filename: "glute-bridges.png",
+    motion_file: "glute-bridges.gif",
+    description:
+      "Glute bridges are a exercise where you lie on your back, bend your knees, and lift your hips off the ground, engaging and strengthening the gluteal muscles.",
+  },
+  {
+    title: "Pull-Ups",
     difficulty: "Hard",
     exhibition_date: "November 3, 2016",
     muscleGroup_id: 3,
-    filename: "pull-up.jpg",
+    filename: "pull-up.png",
     motion_file: "pull-up.gif",
     description:
       "Pull-ups are a challenging upper body exercise targeting the muscles of the back, arms, and shoulders. ",
@@ -156,37 +216,97 @@ const exerciseData = [
     difficulty: "Hard",
     exhibition_date: "November 3, 2016",
     muscleGroup_id: 3,
-    filename: "lat-pulldown.jpg",
+    filename: "lat-pulldown.png",
     motion_file: "lat-pulldown.gif",
     description:
       "The lat pulldown is a popular strength training exercise engaging the muscles of your upper back and arms.",
   },
   {
-    title: "Bent over row",
+    title: "Bent Over Row",
     difficulty: "Moderate",
     exhibition_date: "November 3, 2016",
     muscleGroup_id: 3,
-    filename: "bent-over-row.jpg",
+    filename: "bent-over-row.png",
     motion_file: "bent-over-row.gif",
     description:
       "The bent over row is a compound exercise primarily targets the muscles of the back, including the latissimus dorsi (lats), rhomboids, and traps.",
   },
   {
-    title: "Underhand Chin-ups",
+    title: "Underhand Chin-Ups",
     difficulty: "Moderate",
     exhibition_date: "November 3, 2016",
     muscleGroup_id: 3,
-    filename: "underhand-chin-up.jpg",
+    filename: "underhand-chin-up.png",
     motion_file: "underhand-chin-up.gif",
     description:
       "The underhand chin-up is a variation of the traditional chin-up exercise targeting the muscles of the back, biceps, and shoulders.",
+  },
+  {
+    title: "Dumbbell Row",
+    difficulty: "Moderate",
+    exhibition_date: "November 3, 2016",
+    muscleGroup_id: 3,
+    filename: "dumbbell-row.png",
+    motion_file: "dumbbell-row.gif",
+    description:
+      "Dumbbell rows are a compound exercise where you bend forward, support your body with one hand on a bench, and pull a dumbbell up to your side, targeting the back muscles, particularly the lats and rhomboids.",
+  },
+  {
+    title: "Hyperextensions",
+    difficulty: "Easy",
+    exhibition_date: "November 3, 2016",
+    muscleGroup_id: 3,
+    filename: "hyperextensions.png",
+    motion_file: "hyperextensions.gif",
+    description:
+      "Hyperextensions, also known as back extensions, are an exercise where you lie face down on a hyperextension bench or stability ball and lift your upper body upward, targeting the muscles of the lower back.",
+  },
+  {
+    title: "Inverted Row",
+    difficulty: "Easy",
+    exhibition_date: "November 3, 2016",
+    muscleGroup_id: 3,
+    filename: "inverted-row.jpg",
+    motion_file: "inverted-row.gif",
+    description:
+      "Inverted rows, also known as bodyweight rows or horizontal pulls, are an exercise where you lie under a bar or suspension trainer and pull your chest up towards the bar, targeting the back muscles.",
+  },
+  {
+    title: "One Arm Dumbbell Row",
+    difficulty: "Moderate",
+    exhibition_date: "November 3, 2016",
+    muscleGroup_id: 3,
+    filename: "one-arm-dumbbell-row.png",
+    motion_file: "one-arm-dumbbell-row.gif",
+    description:
+      "One-arm dumbbell rows are an exercise where you stabilize your body with one hand on a bench while pulling a dumbbell up to your side with the opposite arm, targeting the back muscles, especially the lats and rhomboids.",
+  },
+  {
+    title: "Underhand Bent Over Row",
+    difficulty: "Moderate",
+    exhibition_date: "November 3, 2016",
+    muscleGroup_id: 3,
+    filename: "underhand-bent-over-row.jpg",
+    motion_file: "underhand-bent-over-row.gif",
+    description:
+      "Underhand bent over rows are a variation of the bent over row exercise where you grip the barbell or dumbbells with an underhand grip, bend forward, and row the weight towards your lower chest, targeting the upper back and biceps.",
+  },
+  {
+    title: "Dumbbell Pull-Over",
+    difficulty: "Moderate",
+    exhibition_date: "July 4, 2020",
+    muscleGroup_id: 3,
+    filename: "dumbbell-pullover.jpg",
+    motion_file: "dumbbell-pullover.gif",
+    description:
+      "Dumbbell pull-overs are an exercise where you lie on a bench, hold a dumbbell above your chest, and lower it behind your head in a controlled motion, engaging the chest, back, and shoulder muscles.",
   },
   {
     title: "Tricep Kickback",
     difficulty: "Easy",
     exhibition_date: "December 24, 2020",
     muscleGroup_id: 4,
-    filename: "tricep-kickback.jpg",
+    filename: "tricep-kickback.png",
     motion_file: "tricep-kickback.gif",
     description:
       "Tricep kickbacks are an isolation exercise primarily targets the triceps brachii, the muscles on the back of the upper arm.",
@@ -196,7 +316,7 @@ const exerciseData = [
     difficulty: "Easy",
     exhibition_date: "January 20, 2018",
     muscleGroup_id: 4,
-    filename: "barbell-curl.jpg",
+    filename: "barbell-curl.png",
     motion_file: "barbell-curl.gif",
     description:
       "The barbell curl primarily targets the biceps brachii, the muscles on the front of the upper arm.",
@@ -206,7 +326,7 @@ const exerciseData = [
     difficulty: "Easy",
     exhibition_date: "January 20, 2018",
     muscleGroup_id: 4,
-    filename: "hammer-curl.jpg",
+    filename: "hammer-curl.png",
     motion_file: "hammer-curl.gif",
     description:
       "The hammer curl is an arm exercise where you hold dumbbells with a neutral grip primarily targets the brachialis and brachioradialis muscles of the forearms, as well as the biceps.",
